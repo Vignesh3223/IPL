b3aae404d9da1cb111b6505feafb065a0d382419
Vignesh3223 <viketvicky@gmail.com>
2023-05-22 16:59:44 +0530

    commit1

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -40,3 +40,107 @@ $(document).ready(function () {
         }
     });
 });
+
+/*winners and runners table display*/
+function loadTable() {
+    const xhttp = new XMLHttpRequest
+    xhttp.open("GET", "http://localhost:3000/Winners");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var trHTML = "";
+            const objects = JSON.parse(this.responseText);
+            for (let object of objects) {
+                trHTML += "<tr>";
+                trHTML += "<td>" + object["Year"] + "</td>";
+                trHTML += "<td>" + object["Winner"] + "</td>";
+                trHTML += "<td>" + object["Won by"] + "</td>";
+                trHTML += "<td>" + object["Runner Up"] + "</td>";
+                trHTML += "<td>" + object["Venue"] + "</td>";
+                trHTML += "</tr>";
+            }
+            document.getElementById("record1").innerHTML = trHTML;
+        }
+    }
+}
+loadTable();
+
+/*winners and captains table display*/
+function loadTable2() {
+    const xhttp = new XMLHttpRequest
+    xhttp.open("GET", "http://localhost:3000/Captains");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var trHTML = "";
+            const objects = JSON.parse(this.responseText);
+            for (let object of objects) {
+                trHTML += "<tr>";
+                trHTML += "<td>" + object["Year"] + "</td>";
+                trHTML += "<td>" + object["Winner"] + "</td>";
+                trHTML += "<td>" + object["Captain"] + "</td>";
+                trHTML += "<td>" + object["Man of the Match"] + "</td>";
+                trHTML += "<td>" + object["Player of the Series"] + "</td>";
+                trHTML += "</tr>";
+            }
+            document.getElementById("record2").innerHTML = trHTML;
+        }
+    }
+}
+loadTable2();
+
+/*most wins table display*/
+function loadTable3() {
+    const xhttp = new XMLHttpRequest
+    xhttp.open("GET", "http://localhost:3000/Most_Wins");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var trHTML = "";
+            const objects = JSON.parse(this.responseText);
+            for (let object of objects) {
+                trHTML += "<tr>";
+                trHTML += "<td>" + object["Team"] + "</td>";
+                trHTML += "<td>" + object["Trophy"] + "</td>";
+                trHTML += "<td>" + object["Year"] + "</td>";
+                trHTML += "</tr>";
+            }
+            document.getElementById("record3").innerHTML = trHTML;
+        }
+    }
+}
+loadTable3();
+
+/*orange cap winners display*/
+function loadTable4() {
+    const xhttp = new XMLHttpRequest
+    xhttp.open("GET", " http://localhost:3000/Orange_Cap");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            var trHTML = "";
+            const objects = JSON.parse(this.responseText);
+            for (let object of objects) {
+                trHTML += "<tr>";
+                trHTML += "<td>" + object["Year"] + "</td>";
+                trHTML += "<td>" + object["Winner"] + "</td>";
+                trHTML += "<td>" + object["Innings"] + "</td>";
+                trHTML += "<td>" + object["Runs"] + "</td>";
+                trHTML += "<td>" + object["Highest Score"] + "</td>";
+                trHTML += "<td>" + object["Average"] + "</td>";
+                trHTML += "<td>" + object["Strike Rate"] + "</td>";
+                trHTML += "<td>" + object["50s"] + "</td>";
+                trHTML += "<td>" + object["100s"] + "</td>";
+                trHTML += "<td>" + object["4s"] + "</td>";
+                trHTML += "<td>" + object["6s"] + "</td>";
+                trHTML += "</tr>";
+            }
+            document.getElementById("record4").innerHTML = trHTML;
+        }
+    }
+}
+loadTable4();
