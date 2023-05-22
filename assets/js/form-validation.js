678fe3342f01a3ef0fa82a2b556adead687f5859
Vignesh3223 <viketvicky@gmail.com>
2023-05-22 11:19:35 +0530

    commit1

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,37 +1,4 @@
 /*Form validation and response*/
-// function comment() {
-//     var name = document.getElementById("name").value;
-//     var email = document.getElementById("email").value;
-//     var comments = document.getElementById("comments").value;
-//     var letters = /^([A-Za-z ]{2,20})+$/;
-//     var filter = /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/;
-//     if (name == "") {
-//         alert("Please enter a Name");
-//         return false;
-//     }
-//     else if (!(letters).test(name)) {
-//         alert("Name must contain only alphabets");
-//         return false;
-//     }
-//     else if(email == ""){
-//         alert("Please enter a valid E-mail");
-//         return false;
-//     }
-//     else if(!(filter).test(email)){
-//         alert("E-mail must be in the proper format");
-//         return false;
-//     }
-//     else if(comments == ""){
-//         alert("Please enter some comments")
-//         return false;
-//     }
-//     else{
-//         alert("THANK YOU FOR YOUR COMMENTS"); 
-//         document.getElementById("comment").reset();
-//         return false;
-//     }
-//  }
-
 $(document).ready(function () {
     $("#comment").validate({
         rules: {
