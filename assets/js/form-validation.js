89d1d0d5d0712c552fd09092b238eddf650503f1
Vignesh3223 <viketvicky@gmail.com>
2023-05-25 13:04:19 +0530

    commit1

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,45 +1,59 @@
 /*Form validation and response*/
-$(document).ready(function () {
-    $("#comment").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2,
-                maxlength: 20,
-                pattern: /^[A-Za-z ]+$/
-            },
-            email: {
-                required: true,
-                email: true,
-                pattern: /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/
-            },
-            comments: {
-                required: true
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter a Name",
-                minlength: "Name must be at least 2 characters long",
-                maxlength: "Name must not exceed 20 characters",
-                pattern: " Name must contain only alpabets"
-            },
-            email: {
-                required: "Please enter a valid Email",
-                email: "Please enter a valid Email",
-                pattern: "E-mail address should be in proper format"
-            },
-            comments: {
-                required: "Please enter some comments"
-            }
-        },
-        submitHandler: function (form) {
-            alert("THANK YOU FOR YOUR COMMENTS");
-            form.reset();
-            return false;
-        }
-    });
-});
+// $(document).ready(function () {
+//     $("#comment").validate({
+//         rules: {
+//             name: {
+//                 required: true,
+//                 minlength: 2,
+//                 maxlength: 20,
+//                 pattern: /^[A-Za-z ]+$/
+//             },
+//             email: {
+//                 required: true,
+//                 email: true,
+//                 pattern: /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/
+//             },
+//             comments: {
+//                 required: true
+//             }
+//         },
+//         messages: {
+//             name: {
+//                 required: "Please enter a Name",
+//                 minlength: "Name must be at least 2 characters long",
+//                 maxlength: "Name must not exceed 20 characters",
+//                 pattern: " Name must contain only alpabets"
+//             },
+//             email: {
+//                 required: "Please enter a valid Email",
+//                 email: "Please enter a valid Email",
+//                 pattern: "E-mail address should be in proper format"
+//             },
+//             comments: {
+//                 required: "Please enter some comments"
+//             }
+//         },
+//     });
+// });
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
 
 function feedback() {
     const name = document.getElementById("name").value;
