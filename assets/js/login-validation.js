c779d5b51a79c6a02fc8fabc1f7f1f7b70c99f7c
Vignesh3223 <viketvicky@gmail.com>
2023-05-25 10:49:39 +0530

    commit1

--- assets/js/login-validation.js
+++ assets/js/login-validation.js
@@ -1,166 +1,125 @@
-$(document).ready(function () {
-    $("#login-form").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2,
-                maxlength: 20,
-                pattern: /^[A-Za-z ]+$/
-            },
-            password: {
-                required: true,
-                minlength: 8,
-                maxlength: 16,
-                pattern: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#@$])/
+function validateLogin() {
+    Swal.fire({
+        title: "LOGIN",
+        html:
+            '<input id="id" type="hidden">' +
+            '<input id="name" class="swal2-input" placeholder="Enter Username" required>' +
+            '<input id="password" class="swal2-input" placeholder="Enter Password" required>',
+        focusConfirm: false,
+        showCancelButton: true,
+        cancelButtonColor: '#d33',
+        preConfirm: () => {
+            const uname = document.getElementById("name").value;
+            const password = document.getElementById("password").value;
+            const namePattern = /^[A-Za-z ]{2,}$/;
+            const passwordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#@$%&*_]).{8,16}$/;
+            if (!uname || !password) {
+                return Swal.showValidationMessage("Please fill in all the fields");
             }
-        },
-        messages: {
-            name: {
-                required: "Please enter the Username",
-                minlength: "Username must be atleast 2 characters long",
-                maxlength: "Username must not exceed 20 characters",
-                pattern: "Username must contain only alpabets"
-            },
-            password: {
-                required: "Please enter a valid Password",
-                minlength: "Password must be at least 8 characters long",
-                maxlength: "Password must not exceed 16 charcters",
-                pattern: "Password must follow conditions"
+            else if (!namePattern.test(uname)) {
+                return Swal.showValidationMessage("Name must exceed 2 characters and contain only alphabets");
             }
+            else if (!passwordPattern.test(password)) {
+                return Swal.showValidationMessage("Password must contain 8-16 characters with at least one uppercase letter, one lowercase letter, one digit, and one symbol");
+            }
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "http://localhost:3000/Users");
+            xhttp.send();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    const users = JSON.parse(this.responseText);
+                    let validCredentials = false;
+                    for (const validUser of users) {
+                        if (validUser['username'] == uname && validUser['password'] == password) {
+                            validCredentials = true;
+                            break;
+                        }
+                    }
+                    if (validCredentials) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Login Successful',
+                            text: 'Redirecting to index.html...',
+                            showConfirmButton: false,
+                            timer: 2000,
+                            timerProgressBar: true
+                        }).then(() => {
+                            window.location.replace("index.html");
+                        });
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Login Failed',
+                            text: 'Invalid username or password, SignUp First',
+                            confirmButtonText: 'OK'
+                        });
+                    }
+                }
+            };
         }
     });
-})
-$(document).ready(function () {
-    $('#signup-form').validate({
-        rules: {
-            fname: {
-                required: true,
-                minlength: 2,
-                maxlength: 20,
-                pattern: /^[A-Za-z ]+$/
-            },
-            email: {
-                required: true,
-                email: true,
-                pattern: /^[a-z0-9]+@[a-z]+\.[a-z]{2,4}$/
-            },
-            passwd: {
-                required: true,
-                minlength: 8,
-                maxlength: 16,
-                pattern: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#@$])/
-            },
-            confirmPassword: {
-                required: true,
-                equalTo: '#passwd'
-            },
-            phone: {
-                required: true,
-                minlength: 10,
-                pattern: /^\d{10}$/
-            }
-        },
-        messages: {
-            fname: {
-                required: "Please enter your full name",
-                minlength: "Name must be atleast 2 characters long",
-                maxlength: "Name must not exceed 20 characters",
-                pattern: "Name must contain only alpabets"
-            },
-            email: {
-                required: "Please enter your email",
-                email: "Please enter a valid email address",
-                pattern: "E-mail address should be in proper format"
-            },
-            passwd: {
-                required: "Please enter a password",
-                minlength: "Password must be at least 8 characters long",
-                maxlength: "Password must not exceed 16 charcters",
-                pattern: "Password must follow conditions"
-            },
-            confirmPassword: {
-                required: "Please confirm your password",
-                equalTo: "Passwords do not match"
-            },
-            phone: {
-                required: "Please enter your phone number",
-                minlength: "Phone number must be at least 10 characters long",
-                pattern: "Phone umber must contain only digits"
-            }
-        },
-    })
-})
-function validateLogin() {
-    const uname = document.getElementById("name").value;
-    const password = document.getElementById("password").value;
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "http://localhost:3000/Users");
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            const users = JSON.parse(this.responseText);
-            let validCredentials = false;
-            for (const validUser of users) {
-                if (validUser['username'] == uname && validUser['password'] == password) {
-                    validCredentials = true;
-                    break;
-                }
-            }
-            if (validCredentials) {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Login Successful',
-                    text: 'Redirecting to index.html...',
-                    showConfirmButton: false,
-                    timer: 2000,
-                    timerProgressBar: true
-                }).then(() => {
-                    window.location.replace("index.html");
-                });
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Login Failed',
-                    text: 'Invalid username or password',
-                    confirmButtonText: 'OK'
-                });
-            }
-        }
-    };
 }
 
+
 function validateSignUp() {
-    const fname = document.getElementById("fname").value;
-    const email = document.getElementById("email").value;
-    const passwd = document.getElementById("passwd").value;
-    const confirmPassword = document.getElementById("confirmPassword").value;
-    const phone = document.getElementById("phone").value;
-    console.log("hi")
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://localhost:3000/Users");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(
-        JSON.stringify({
-            fname: fname,
-            email: email,
-            passwdn: passwd,
-            confirmPassword: confirmPassword,
-            phone: phone,
-        })
-    );
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            const users = JSON.parse(this.responseText);
-            Swal.fire({
-                icon: 'success',
-                title: 'Login Successful',
-                text: 'Redirecting to index.html...',
-                showConfirmButton: false,
-                timer: 2000,
-                timerProgressBar: true
-            }).then(() => {
-                window.location.replace("index.html");
-            });
-        }
-    };
+    Swal.fire({
+        title: "CREATE ACCOUNT",
+        html:
+            '<input id="id" type="hidden">' +
+            '<input id="fname" class="swal2-input" placeholder="Enter First Name" required>' +
+            '<input id="email" class="swal2-input" placeholder="Enter Email" required>' +
+            '<input id="passwd" class="swal2-input" placeholder="Enter Password" required>' +
+            '<input id="confirmPassword" class="swal2-input" placeholder="Re-enter Password" required>' +
+            '<input id="phone" class="swal2-input" placeholder="Enter Phone number" required>',
+        focusConfirm: false,
+        showCancelButton: true,
+        cancelButtonColor: '#d33',
+        preConfirm: () => {
+            const fname = document.getElementById("fname").value;
+            const email = document.getElementById("email").value;
+            const passwd = document.getElementById("passwd").value;
+            const confirmPassword = document.getElementById("confirmPassword").value;
+            const phone = document.getElementById("phone").value;
+            const namePattern = /^[A-Za-z ]{2,}$/;
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const passwordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#@$%&*_]).{8,16}$/;
+            const phonePattern = /^\+?\d+$/;
+            if (!fname || !email || !passwd || !confirmPassword || !phone) {
+                return Swal.showValidationMessage("Please fill in all the fields");
+            } else if (!namePattern.test(fname)) {
+                return Swal.showValidationMessage("Please enter a valid first name with at least 2 characters and only alphabets");
+            } else if (!emailPattern.test(email)) {
+                return Swal.showValidationMessage("Please enter a valid email address");
+            } else if (!passwordPattern.test(passwd)) {
+                return Swal.showValidationMessage("Please enter a valid password with 8-16 characters, including at least one uppercase letter, one lowercase letter, one digit, and one symbol");
+            } else if (passwd !== confirmPassword) {
+                return Swal.showValidationMessage("Passwords do not match");
+            } else if (!phonePattern.test(phone)) {
+                return Swal.showValidationMessage("Please enter a valid phone number");
+            }
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("POST", "http://localhost:3000/Users");
+            xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhttp.send(
+                JSON.stringify({
+                    username: fname,
+                    email: email,
+                    password: passwd,
+                    confirmPassword: confirmPassword,
+                    phone: phone,
+                })
+            );
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    const users = JSON.parse(this.responseText);
+                    console.log(this.responseText);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Sign Up Successful',
+                        text: 'Account Created Successfully, You can Login Now!',
+                    });
+                }
+            };
+        },
+    });
 }
